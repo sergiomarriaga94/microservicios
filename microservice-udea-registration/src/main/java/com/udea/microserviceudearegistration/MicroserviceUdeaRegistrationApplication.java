0972466f3fb03d63c4bb2b4d8b967f8d39dc87f0
Sergio marriaga <chechoitpb@gmail.com>
2017-05-15 16:47:34 -0500

    construido con dependencias

---
+++ microservice-udea-registration/src/main/java/com/udea/microserviceudearegistration/MicroserviceUdeaRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.udea.microserviceudearegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUdeaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUdeaRegistrationApplication.class, args);
+	}
+}
